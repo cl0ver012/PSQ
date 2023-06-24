5e18f4227cc7d7a8c696e7c9a4ecbca48a257cb5
cl0ver012 <goodcristina2023@gmail.com>
2023-06-24 04:53:21 -0700

    Fix: First Problem table

--- 1.py
+++ 1.py
@@ -8,20 +8,19 @@ def prob_rain_more_than_n(p: Sequence[float], n: int) -> float:
     days = len(p)
     
     # Initialize the dp table
-    dp = [[0.0 for _ in range(days + 1)] for _ in range(days+1)]
+    dp = [[0.0 for _ in range(n+1)] for _ in range(days+1)]
     
     # Base case: There is a 100% chance of having 0 rainy days in the first 0 days
     dp[0][0] = 1.0
     
     # Fill the dp table
     for i in range(1, days+1):
-        for j in range(days+1):
+        for j in range(n+1):
             dp[i][j] = dp[i-1][j] * (1-p[i-1]) # Probability of not raining on day i
             if j > 0:
                 dp[i][j] += dp[i-1][j-1] * p[i-1] # Probability of raining on day i
     
     # Calculate the probability of having at least n rainy days
-    result = sum(dp[days][n:])
+    result = 1 - sum(dp[days][:n])
     
     return result
-
